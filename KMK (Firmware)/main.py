5dc645678d8506cbeb65628d3cad1ac8d39e4077
Skittish <115852542+nitishisskittish@users.noreply.github.com>
2026-04-08 21:01:07 -0400

    So...it's been a while A lot of things went wrong with assembly, just to name a few: The 3D printed case was to tight, and the pcb didn't fit inside The upwards facing LED's were impossible to solder (skill issue, but also a footprint issue) The diode footprints were NOT compatible with the diodes I got (as far I could tell while trying to solder) And worst of all, the whole thing didn't even work.
    Until now,
    With this new firmware the macropad now works. Buttons (well only one right now, haven't given the rest a function), rotary dial, the screen, the LEDs backlighting the keys, all of it.
    It did take me a while to find time, but I think I can now say that this project wasn't a complete failure
    Plus I learnt a lot, and I definetly will not be making the same mistakes on my next project
    
    This was quite the commit message huh, thanks for reading this far if you did.

--- KMK (Firmware)/main.py
+++ KMK (Firmware)/main.py
@@ -1,69 +1,51 @@
-# Future Update List:
-# Make it so that holding down the encoder and turning adjusts RGB Brightness.
-# Make it so that holding down rightmost key and turning encoder adjusts RGB Color.
-# Make it so that OLED Displays Brightness level when being adjusted 
-
 import board
-import digitalio
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
-from kmk.matrix import DiodeOrientation
+from kmk.quickpin import QuickPin
+from kmk.modules.encoder import Encoder
+from kmk.modules.layers import Layers
 from kmk.extensions.rgb import RGB, RGBModes
 from kmk.extensions.display.volume import VolumeDisplay
 from kmk.extensions.display import Display
-from kmk.modules.encoder import Encoder
-from kmk.modules.layers import Layers
 from kmk.extensions.media_keys import MediaKeys
 
 keyboard = KMKKeyboard()
 
 keyboard.extensions.append(MediaKeys())
 
-# RGB LEDs:
 rgb = RGB(
-    pixel_pin=board.D7,
+    pixel_pin=board.D7,   # Adjust if needed
     num_pixels=16,
-    hue=0,
-    sat=0, 
-    val=255, 
     mode=RGBModes.STATIC_COLOR,
     val_default=72
 )
 keyboard.extensions.append(rgb)
 
-# OLED Volume Display
 display_ext = Display(
-    i2c_port=board.I2C(),
+    i2c_port=board.I2C(sda=board.D0, scl=board.D1),
     display=VolumeDisplay(),
 )
 keyboard.extensions.append(display_ext)
 
-# Layers (Right now I only have one, but I may add more later)
 keyboard.modules.append(Layers())
 
-# Rotary Encoder
-keyboard.modules.append(Encoder(
-    pins=((board.D9, board.D10, None),),
-    tap_max=1000, 
-    on_clockwise=KC.VOLU, 
-    on_ccwise=KC.VOLD, 
-))
+keyboard.modules.append(
+    Encoder(
+        pins=((board.D4, board.D5, None),),  # D4/D5 are free for GPIO input
+        on_clockwise=KC.VOLU,
+        on_ccwise=KC.VOLD,
+    )
+)
 
+keyboard.matrix = QuickPin(
+    rows=(board.D2, board.D3),             # GP28, GP29
+    cols=(board.D6, board.D7, board.D8, board.D9),  # GP0, GP1, GP2, GP4
+)
 
-# Key Matrix
-keyboard.row_pins = (board.D6, board.D8)
-keyboard.col_pins = (board.D0, board.D1, board.D2, board.D3)
-keyboard.diode_orientation = DiodeOrientation.COL2ROW
+keyboard.keymaps = [[
+    KC.MPRV, KC.W, KC.MNXT, KC.MUTE,
+    KC.A, KC.S, KC.D, KC.LGUI(KC.S)
+]]
 
-
-# Keymap
-KEYMAP = [
-    [
-        KC.MPRV, KC.W,    KC.MNXT, KC.MUTE,
-        KC.A,    KC.S,    KC.D,    KC.LGUI(KC.S) # Custom shortcut key for opening spotify on my system
-    ]
-]
-keyboard.keymaps = KEYMAP
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     keyboard.go()
